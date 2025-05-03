638ba39b4ade91e3219a93592c00fb43c2d9f3da
Minnakhmetov Almaz <mastera4ek2017@gmail.com>
2025-05-03 19:04:33 +0300

    Add ClientStore in app_data in auth_service + sqlx query files

--- services/auth-service/src/startup.rs
+++ services/auth-service/src/startup.rs
@@ -6,7 +6,7 @@ use actix_web::{cookie::Key, dev::Server, web, App, HttpResponse, HttpServer};
 use secrecy::ExposeSecret;
 use tracing_actix_web::TracingLogger;
 
-use crate::{auth::{code_store::CodeStore, jwt::JwtService, token_store::TokenStore}, config::Settings, routes::{auth, oauth}, services::user::UserService, utils::session_middleware};
+use crate::{auth::{client_store::ClientStore, code_store::CodeStore, jwt::JwtService, token_store::TokenStore}, config::Settings, routes::{auth, oauth}, services::user::UserService, utils::session_middleware};
 
 pub fn run(
     listener: TcpListener,
@@ -14,12 +14,14 @@ pub fn run(
     token_store: TokenStore,
     code_store: CodeStore,
     user_service: UserService,
+    client_store: ClientStore,
     redis_store: RedisSessionStore,
     config: Settings,
 ) -> Result<Server, std::io::Error> {
     let jwt_service = web::Data::new(jwt_service);
     let token_store = web::Data::new(token_store);
     let code_store = web::Data::new(code_store);
+    let client_store = web::Data::new(client_store);
     let user_service = web::Data::new(user_service);
 
     let secret_key = if let Some(key) =  config.session.secret_key {
@@ -36,6 +38,7 @@ pub fn run(
             .app_data(token_store.clone())
             .app_data(code_store.clone())
             .app_data(user_service.clone())
+            .app_data(client_store.clone())
             .route("/health", web::to(HttpResponse::Ok))
             .configure(auth::configure_routes)
             .configure(oauth::configure_routes)
